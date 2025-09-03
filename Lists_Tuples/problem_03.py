250ee2a5b074a75cb1ca0aa08fdeedabbad89aff
Biradar331 <biradarpavan843@gmail.com>
2025-09-03 09:59:04 +0530

    added more problems on lists and tuples

---
+++ Lists_Tuples/problem_03.py
@@ -0,0 +1,22 @@
+#another problem on palindrome
+name=['m','a','a','m']
+name1=name.copy()
+name.reverse()
+
+if name1 ==name :
+    print("palindrome")
+else:
+    print("not a palindrome")
+
+
+#Problem_02
+
+#another problem on palindrome
+name=['m','a','a','m','a']
+name1=name.copy()
+name.reverse()
+
+if name1 ==name :
+    print("palindrome")
+else:
+    print("not a palindrome")
---
+++ Lists_Tuples/problem_04.py
@@ -0,0 +1,14 @@
+#Write a program to count the number of studets with grade 'A' in the following tuble
+grades=('C','D','A','A','B','B','A')
+grade1=grades.count('A')
+print(grade1)
+
+#It will print the number of grade 'A' present in the tuple
+
+#problem_4.1
+
+#Store the above values in a list and sort them in a form 'A'to'D'
+
+grade=['C','D','A','A','B','B','A']
+grade.sort()
+print(grade)
