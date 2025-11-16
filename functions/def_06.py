761afba41bd4f97b92553327587f461f14070344
Biradar331 <biradarpavan843@gmail.com>
2025-11-16 23:53:02 +0530

    problem-05

--- functions/def_06.py
+++ functions/def_06.py
@@ -1,6 +1,9 @@
+
 def factorial(N,result):
     if N==0:
         print(result)
         return
+    
     return factorial(N-1,result*N)
+
 factorial(5,1)
--- pyhton_practice/problem_05.py
+++ pyhton_practice/problem_05.py
@@ -2,3 +2,6 @@
 word="pyhton"
 for i in range(len(word)-1,-1,-1):
     print(word[i], end=" ")
+    
+# It will print the "python" word in reverse order
+
