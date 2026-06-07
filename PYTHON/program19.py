5827cb319c5b47490bcab5e42de9f1c2b38aaaad
Pavan Biradar <biradarpavan843@gmail.com>
2026-06-07 20:05:58 +0530

    recursion problem on product of digits

---
+++ PYTHON/program19.py
@@ -0,0 +1,12 @@
+# program to calculate the product of positive digits using recursion
+
+def product_digits(n):
+    if n<0:
+        raise ValueError ("Can't calculate for negative digits")
+    if n==0:
+        return 0
+    if n<10:
+        return n
+    return (n%10)*product_digits(n//10)
+result=product_digits(5)
+print("the product of given digits are:", result)
