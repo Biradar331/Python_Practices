aa57fb973e51ff8ea48ae0fe644d1e3c859d211a
Pavan Biradar <biradarpavan843@gmail.com>
2026-06-25 17:54:45 +0530

    practiced a problem on multiplication table using loop

---
+++ PYTHON/program20.py
@@ -0,0 +1,15 @@
+# python program to display a multiplication table using for loop
+
+n=int(input("enter a table number:"))
+for i in range (1,11):
+    print(n,"×", i, "=", n*i)
+
+
+# python program to display a multiplication tale using while loop
+
+n=int(input("enter a multiplication table number:"))
+i=1
+while i <= 10:
+    print(n,"×", i, "=", n*i)
+    i=i+1
+    
