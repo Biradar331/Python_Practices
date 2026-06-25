6adac4521c799126bd09d20f783abe9c4f6a3313
Pavan Biradar <biradarpavan843@gmail.com>
2026-06-25 18:26:06 +0530

    made some changes in the fibonacci sequence

--- PYTHON/program21.py
+++ PYTHON/program21.py
@@ -3,13 +3,18 @@
 num=int(input("Enter a number:"))
 a=0
 b=1
-print(a)
-print(b)
 
-for i in range(1,num+1):
-    c=a+b
-    a=b
-    b=c
-    print(c)
+if num<=1:
+    print(a)
+else:
+    print(a)
+    print(b)
+
+    for i in range(3,num+1):
+        c=a+b
+        a=b
+        b=c
+        print(c)
+
 
 
