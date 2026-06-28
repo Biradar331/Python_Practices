936a8700ef3ab4002691356dadf4ba76cd7f7249
Pavan Biradar <biradarpavan843@gmail.com>
2026-06-28 22:37:50 +0530

    written a problem to check if a number is armstrong number or not in an interval

--- PYTHON/program25.py
+++ PYTHON/program25.py
@@ -13,3 +13,23 @@ if n==result:
     print(f"The number {n} is a armstrong number")
 else:
     print(f"The number {n} is not a armstrong number")
+
+## Time complexisty for the above problem is --> O(log10(n))
+
+
+# Python program to check if a number is armstrong number or not in an interval
+
+lower=int(input("Enter a lower limit:"))
+upper=int(input("Enter a upper limit:"))
+
+
+for num in range(lower, upper+1):
+    result=0
+    temp=num
+    order=len(str(num))
+    while temp>0:
+        digit= temp%10
+        result= result+digit**order
+        temp=temp//10
+    if num==result:
+        print(num)
