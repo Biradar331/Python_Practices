cf539fd7aa6c4f29247910c57612d4e28aebbe32
Pavan Biradar <biradarpavan843@gmail.com>
2026-06-30 23:37:49 +0530

    solved problem to find factors of a number

--- PYTHON/program28.py
+++ PYTHON/program28.py
@@ -1,8 +1,20 @@
-# Python program to print all factors of a given number
+# Python program to print all factors of a given number using loop condition
 
 result=[]
 num=int(input("Enter the number:"))
 for i in range(1,num+1):
     if num%i==0:
         result.append(i)
+print(result)
+
+# Time Complexity would be : O(N) & Space complexity would be O(K) 'k' would be total no. of factors
+
+# python program to print all factors of a number 
+
+result=[]
+num=int(input("Enter the number:"))
+for i in range(1,(num//2)+1):
+    if num%i==0:
+        result.append(i)
+result.append(num)
 print(result)
