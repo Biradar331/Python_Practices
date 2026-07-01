659187b46d3d644bfbc2fc8eca27943e6d3b9f80
Pavan Biradar <biradarpavan843@gmail.com>
2026-07-01 17:58:23 +0530

    solved a problem on frequency map using 'hash_map.get' method

--- PYTHON/program30.py
+++ PYTHON/program30.py
@@ -2,7 +2,7 @@
 num=[1,3,5,7,8,99,7,5,3,4,4,34,34,23,56,77]
 freq_map={}
 
-for i in range(1, len(num)):
+for i in range(0, len(num)):
     if num[i] in freq_map:
         freq_map[num[i]]+=1
     else:
@@ -15,3 +15,12 @@ print(freq_map)
 
 ## Space Complexity of the above problem is --> O(n)
 
+# Problem --02 
+
+nums=[1,3,5,7,8,99,7,5,3,4,4,34,34,23,56,77]
+
+Hash_map={}
+
+for i in range(0, len(nums)):
+    Hash_map [nums[i]]=Hash_map.get(nums[i],0)+1
+print(Hash_map)
