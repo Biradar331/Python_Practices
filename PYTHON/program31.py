f16513d9190d8ca11d6a04cf7ba0ddc9f7f63591
Pavan Biradar <biradarpavan843@gmail.com>
2026-07-02 23:31:43 +0530

    solved the problem to find numbers divisible by another number

--- PYTHON/program31.py
+++ PYTHON/program31.py
@@ -1,6 +1,13 @@
-# Program to find numbers divisible by another number
+# Program to find numbers divisible by another number using for loop
 
 num=int(input("Enter a number:"))
 for i in range(1, 101):
     if i%num ==0:
         print(i)
+
+
+# Solution 2 using lambda function or filter()
+
+num=int(input("Enter a number:"))
+divisible_numbers = list(filter(lambda x: x % num == 0, range(1, 101)))
+print(divisible_numbers)
