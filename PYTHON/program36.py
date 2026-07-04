903308207cf93dcc00f242bfdc16f5b73e9ac26d
Pavan Biradar <biradarpavan843@gmail.com>
2026-07-04 23:18:50 +0530

    program to display deck of cards

---
+++ PYTHON/program36.py
@@ -0,0 +1,12 @@
+# Python program to shuffle deck of cards
+
+import random, itertools
+
+deck = list(itertools.product(range(1, 14), ['Spades', 'Hearts', 'Diamonds', 'Clubs']))
+random.shuffle(deck)
+print(deck)
+
+for i in range(5):
+    print(deck[i][0], "of", deck[i][1])
+
+    
