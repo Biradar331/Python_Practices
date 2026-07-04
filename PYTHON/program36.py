65e1dcb27ca92a977c08fab06895918cdc38c931
Pavan Biradar <biradarpavan843@gmail.com>
2026-07-04 23:25:40 +0530

    written a program to print a calender

--- PYTHON/program36.py
+++ PYTHON/program36.py
@@ -9,4 +9,13 @@ print(deck)
 for i in range(5):
     print(deck[i][0], "of", deck[i][1])
 
-    
+
+# Python program to display calender
+
+import calendar
+
+year=int(input("Enter year:"))
+month=int(input("Enter month:"))
+
+calendar=calendar.month(year,month)
+print(calendar)
